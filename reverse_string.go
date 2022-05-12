8795765ddbcd8adc51e17bc08fc4de5d60688483
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-05-12 19:42:53 +0200

    add task1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
 	return output
 }
