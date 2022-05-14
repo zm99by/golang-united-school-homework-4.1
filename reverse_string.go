4c73ea26da4093d95df4b586b9b6f23a38bc79c5
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-05-14 13:08:18 +0200

    modified:   reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -1,10 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	runes := []rune(input)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return output
+	r := []rune(input)
+	c := make([]rune, len(r))
 
+	for k, v := range r {
+		c[len(r)-k-1] = v
+	}
+
+	return string(c)
 }
